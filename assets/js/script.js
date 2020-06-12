fbd5c5985f1186a43fefa8546f52c324ae8f8c38
Tolga Özdemir <4160141@RTS-365.dk>
2020-06-12 13:00:17 +0200

    add dropdown and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,31 @@ let mainNav = document.getElementById('uList');
 let navBarToggle = document.querySelector("#checkboxLabel");
 let checbox = document.querySelector("#checkbox");
 
+
+// Checkbox unchecked when site width is over 767px
   var mq = window.matchMedia( "(max-width: 767px)" );
-  if (mq.matches) {
-    
-  }
+  if (mq.matches) {}
   else {
     checbox.checked = false
   }
 
+// Give class active when you click on the Burger Menu
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle('active');
 });
+
+
+// Dropdown JAMSTACK, give flex-style when pressed and remove when pressed again.
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "flex") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "flex";
+    }
+  });
+}
