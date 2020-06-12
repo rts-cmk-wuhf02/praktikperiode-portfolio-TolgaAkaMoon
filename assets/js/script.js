f5b5abfa829966631c8013b435080cd46d830e84
Tolga Özdemir <4160141@RTS-365.dk>
2020-06-12 11:15:39 +0200

    add checkbox checked false when on desktop version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 let mainNav = document.getElementById('uList');
 let navBarToggle = document.querySelector("#checkboxLabel");
-console.log(navBarToggle)
+let checbox = document.querySelector("#checkbox");
+
+  var mq = window.matchMedia( "(max-width: 767px)" );
+  if (mq.matches) {
+    
+  }
+  else {
+    checbox.checked = false
+  }
 
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle('active');
-  console.log("hello")
 });
