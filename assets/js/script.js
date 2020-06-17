c730e756fafcc652b1c7de957f50e24670c4ca55
TolgaAkaMoon <4160141@RTS-365.dk>
2020-06-17 13:23:09 +0200

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,47 @@ let mainNav = document.getElementById('uList');
 let navBarToggle = document.querySelector("#checkboxLabel");
 let checbox = document.querySelector("#checkbox");
 
-let jamstackDesc = document.querySelector(".main__jamDesc");
 
 
 
 // Checkbox unchecked when site width is over 767px
-  var mq = window.matchMedia( "(max-width: 767px)" );
-  if (mq.matches) {}
-  else {
-    checbox.checked = false
-  }
+var mq = window.matchMedia( "(max-width: 767px)" );
+if (mq.matches) {}
+else {
+  checbox.checked = false
+}
 
 // Give class active when you click on the Burger Menu
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle('active');
   if (mainNav.classList = active) {
-
+    
   }
 });
 
 
 // Dropdown JAMSTACK, give flex-style when pressed and remove when pressed again.
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
+let dropdown = document.getElementsByClassName("dropdown-btn");
+let i;
+let jamstackDesc = document.querySelector(".main__jamDesc");
+let jamstackDrop = document.querySelector(".dropdown-container");
+let jamstackTitle = document.querySelector(".jamstack__heading1");
+let JamstackText = [{
+  JavaScript: 'Jeg har arbejdet med JavaScript i over 2 år nu, og jeg har lært en masse omkring DOM Manupulation.'
+},
+{
+  API: 'Spotify har et API som jeg har arbejdet med, man skal dov have premium som er noget lort.'
+},
+{
+  HTML: 'Jeg har arbejdet med HTML i et stykek tid, blah blah blah, jeg er super god til at kode og så videre, html html blah flere år endu'
+}]
+
+
+
+jamstackDesc.innerHTML = JamstackText[0].JavaScript
+console.log(JamstackText[0].JavaScript)
+
+
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
     this.classList.toggle("activeJob");
@@ -38,5 +56,3 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
-
-let dropdwn = document.querySelector(".dropdown-container");
